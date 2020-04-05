422086962abbfee996f258a779f5bb6594a021ef
jonz94 <jody16888@gmail.com>
2020-04-05 23:45:20 +0800

    🔁 refactor: update dotfiles before `zplug update`

--- zsh/plugins/update.zsh
+++ zsh/plugins/update.zsh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 dug() {
-  # update zsh plugins via zplug
-  zplug update
-  echo
-
   # update dotfiles
   echo -e ${BLUE}"[Updating]" dotfiles${NORMAL}
   git -C ${DOTS} fetch --all --prune
   git -C ${DOTS} pull
+
+  # update zsh plugins via zplug
+  zplug update
+  echo
 }
