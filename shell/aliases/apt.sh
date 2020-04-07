8bcfb511f85992070430e23e6ea3aef27380e159
jonz94 <jody16888@gmail.com>
2020-04-08 06:55:10 +0800

    ✨ feat(alias): add `apt` aliases on termux emulator

--- shell/aliases/apt.sh
+++ shell/aliases/apt.sh
@@ -7,3 +7,12 @@ if `hash apt-get 2>/dev/null`; then
   alias aud='sudo apt update && apt list --upgradable'
   alias aug='sudo apt upgrade'
 fi
+
+# termux emulator
+if [ -n "`uname -a | grep Android`" ]; then
+  alias i='apt install'
+
+  alias aarm='apt autoremove'
+  alias aud='apt update && apt list --upgradable'
+  alias aug='apt upgrade'
+fi
