f1309657e562dc50130d7f1d9178d5cfdbd08c71
jonz94 <jody16888@gmail.com>
2020-05-27 05:45:39 +0800

    ✨ feat(alias): add `git switch` shell alias

--- shell/aliases/git.sh
+++ shell/aliases/git.sh
@@ -8,5 +8,6 @@ if `hash git 2>/dev/null`; then
   alias gstaa='git stash push --include-untracked'
   alias gstai='git stash push --include-untracked'
   alias gstau='git stash push --include-untracked'
+  alias gsw-='git switch -'
   alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "🚧 WIP: work in progress..."'
 fi
