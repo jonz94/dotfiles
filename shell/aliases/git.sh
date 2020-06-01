530fece67ca83af7377807909d09b3539f96b855
jonz94 <jody16888@gmail.com>
2020-06-01 17:32:08 +0800

    😎 feat(alias): show graph for git reflog command

--- shell/aliases/git.sh
+++ shell/aliases/git.sh
@@ -11,4 +11,9 @@ if `hash git 2>/dev/null`; then
   alias gstau='git stash push --include-untracked'
   alias gsw-='git switch -'
   alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "🚧 WIP: work in progress..."'
+
+  # show graph for git reflog command
+  # Credit: https://gist.github.com/matthewmccullough/988077
+  alias glggr='git log --graph --decorate `git reflog --format=format:%h`'
+  alias glogr='git log --oneline --graph --decorate `git reflog --format=format:%h`'
 fi
