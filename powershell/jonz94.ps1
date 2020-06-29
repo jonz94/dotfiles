de8d7f0e595c792ee97e374657285a23e4888870
jonz94 <jody16888@gmail.com>
2020-06-29 21:39:04 +0800

    ✨ feat(powershell): add some `scoop` aliases

--- powershell/jonz94.ps1
+++ powershell/jonz94.ps1
@@ -238,12 +238,15 @@ function gwip {
 }
 
 # scoop aliases
-function scl { scoop cleanup }
+function i { scoop install $args }
+function scl { scoop cleanup $args }
 function scl* { scoop cleanup * }
+function si { scoop info $args }
 
 Remove-Item Alias:\sls -Force *> $null
 function sls { scoop list }
 
+function ss { scoop search $args }
 function sst { scoop status }
 
 function sup { scoop update $args }
