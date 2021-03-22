ee564f6fb448d463c2cd08fa85e04965d3f7073e
jonz94 <jody16888@gmail.com>
2021-03-23 01:04:46 +0800

    🔥 chore(shell): remove `qmk.zsh`

--- zsh/plugins/qmk.zsh
+++
@@ -1,9 +0,0 @@
-# Add qmk related binary to PATH
-
-if [ -d /usr/local/opt/avr-gcc@8/bin ]; then
-  export PATH="/usr/local/opt/avr-gcc@8/bin:$PATH"
-fi
-
-if [ -d /usr/local/opt/arm-gcc-bin@8/bin ]; then
-  export PATH="/usr/local/opt/arm-gcc-bin@8/bin:$PATH"
-fi
