4f98dc7d4494261dd61ff97bf643e27bc882bb6d
jonz94 <jody16888@gmail.com>
2021-05-14 06:12:40 +0800

    😎 feat(bootstrap): add check for each step before running it

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -10,13 +10,19 @@ if (-Not $(Test-Path "$HOME\dotfiles\local\local.ps1" -PathType Leaf)) {
 }
 
 # Create $PROFILE file if it does not exists
-if (-Not $(Test-Path "$PROFILE" -PathType Leaf)) {
-  New-Item "$PROFILE" -ItemType File -Force *> $null
+if (-Not $(Test-Path $PROFILE -PathType Leaf)) {
+  New-Item $PROFILE -ItemType File -Force *> $null
 }
 
-# Source our custom profiles
-Add-Content "$PROFILE" -Encoding UTF8 -Value '. $HOME\dotfiles\powershell\jonz94.ps1'
-Add-Content "$PROFILE" -Encoding UTF8 -Value '. $HOME\dotfiles\local\local.ps1'
+# Source main profile
+if (-Not $(Select-String -Path $PROFILE -Pattern '^\. \$HOME\\dotfiles\\powershell\\jonz94\.ps1$') ) {
+  Add-Content $PROFILE -Encoding UTF8 -Value '. $HOME\dotfiles\powershell\jonz94.ps1'
+}
+
+# Source local profile
+if (-Not $(Select-String -Path $PROFILE -Pattern '^\. \$HOME\\dotfiles\\local\\local\.ps1$') ) {
+  Add-Content $PROFILE -Encoding UTF8 -Value '. $HOME\dotfiles\local\local.ps1'
+}
 
 # Install oh-my-posh3
 if (-Not $(scoop which oh-my-posh) ) {
@@ -28,28 +34,38 @@ if (-Not $(scoop which nvim) ) {
   scoop install neovim
 }
 
+$initDotVimPath = "$HOME\AppData\Local\nvim\init.vim"
+
 # Create neovim's init.vim if it does not exist
-if (-Not $(Test-Path "$HOME\AppData\Local\nvim\init.vim" -PathType Leaf)) {
-  New-Item "$HOME\AppData\Local\nvim\init.vim" -ItemType File -Force *> $null
+if (-Not $(Test-Path $initDotVimPath -PathType Leaf)) {
+  New-Item $initDotVimPath -ItemType File -Force *> $null
 }
+
+if (-Not $(Select-String -Path $PROFILE -Pattern '^source \$HOME\/dotfiles\/nvim\/jonz94\.vim$') ) {
+  Add-Content $initDotVimPath -Encoding UTF8 -Value 'source $HOME/dotfiles/nvim/jonz94.vim'
+}
+
+$ginitDotVimPath = "$HOME\AppData\Local\nvim\ginit.vim"
 
 # Create neovim's ginit.vim if it does not exists
-if (-Not $(Test-Path "$HOME\AppData\Local\nvim\ginit.vim" -PathType Leaf)) {
-  New-Item "$HOME\AppData\Local\nvim\ginit.vim" -ItemType File -Force *> $null
+if (-Not $(Test-Path $ginitDotVimPath -PathType Leaf)) {
+  New-Item $ginitDotVimPath -ItemType File -Force *> $null
 }
 
-# Source neovim configuration files
-Add-Content "$HOME\AppData\nvim\init.vim" -Encoding UTF8 -Value 'source $HOME/dotfiles/nvim/jonz94.vim'
-Add-Content "$HOME\AppData\nvim\ginit.vim" -Encoding UTF8 -Value 'source $HOME/dotfiles/nvim/jonz94.gvim'
+if (-Not $(Select-String -Path $PROFILE -Pattern '^\source \$HOME\/dotfiles\/nvim\/jonz94\.gvim$') ) {
+  Add-Content $ginitDotVimPath -Encoding UTF8 -Value 'source $HOME/dotfiles/nvim/jonz94.gvim'
+}
+
+$plugDotVimPath = "$HOME\AppData\Local\nvim\autoload\plug.vim"
 
 # Install vim-plug
-if (-Not $(Test-Path "$HOME\AppData\Local\nvim\autoload\plug.vim" -PathType Leaf)) {
+if (-Not $(Test-Path $plugDotVimPath -PathType Leaf)) {
   md ~\AppData\Local\nvim\autoload
   $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   (New-Object Net.WebClient).DownloadFile(
     $uri,
     $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
-      "~\AppData\Local\nvim\autoload\plug.vim"
+      $plugDotVimPath
     )
   )
 }
@@ -67,10 +83,15 @@ if (-Not $(scoop which fnm) ) {
   scoop install fnm
 }
 
-# Install powershell modules
-Install-Module posh-git -Scope CurrentUser
-Install-Module PSFzf -Scope CurrentUser
+# Install posh-git modules
+if (-Not $(Get-InstalledModule).Name.contains('posh-git') ) {
+  Install-Module 'posh-git' -Scope CurrentUser
+}
+
+# Install PSFzf modules
+if (-Not $(Get-InstalledModule).Name.contains('PSFzf') ) {
+  Install-Module 'PSFzf' -Scope CurrentUser
+}
 
 # Load fnm & generate fnm's completions file
-fnm env --use-on-cd | Out-String | Invoke-Expression
-fnm completions --shell powershell > $HOME\dotfiles\powershell\completions\fnm.completions.ps1
+powershell.exe -NoProfile -Command "fnm env --use-on-cd | Out-String | Invoke-Expression; fnm completions --shell powershell > $HOME\dotfiles\powershell\completions\fnm.completions.ps1"
