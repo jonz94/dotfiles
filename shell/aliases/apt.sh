268639cca2e75b7f61ffb0a39cb84851558d5c51
jonz94 <jody16888@gmail.com>
2021-05-16 20:22:31 +0800

    ✨ feat(shell): add `ud` and `ug` aliases for linux

--- shell/aliases/apt.sh
+++ shell/aliases/apt.sh
@@ -6,6 +6,8 @@ if type apt-get &> /dev/null; then
   alias aarm='sudo apt autoremove'
   alias aud='sudo apt update && apt list --upgradable'
   alias aug='sudo apt upgrade'
+  alias ud='sudo apt update && apt list --upgradable'
+  alias ug='sudo apt upgrade'
 fi
 
 # termux emulator
@@ -15,4 +17,6 @@ if [ -n "`uname -a | grep Android`" ]; then
   alias aarm='apt autoremove'
   alias aud='apt update && apt list --upgradable'
   alias aug='apt upgrade'
+  alias ud='apt update && apt list --upgradable'
+  alias ug='apt upgrade'
 fi
