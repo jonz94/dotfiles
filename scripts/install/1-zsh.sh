1669aeab93b37ef15de4204f9a5bcd6276b9f6a2
jonz94 <jody16888@gmail.com>
2021-05-17 02:35:14 +0800

    ✨ feat(bootstrap): add support for termux

--- scripts/install/1-zsh.sh
+++ scripts/install/1-zsh.sh
@@ -1,14 +1,17 @@
 echo 'installing zsh...'
 
-if type apt-get &> /dev/null; then
+if type nvim &> /dev/null; then
+  echo 'zsh has been installed, skip installation.'
+  exit 0
+fi
+
+if type pkg &> /dev/null; then
+  pkg install -y zsh
+elif type apt-get &> /dev/null; then
   sudo apt install -y zsh
-fi
-
-if type brew &> /dev/null; then
+elif type brew &> /dev/null; then
   brew install zsh
-fi
-
-if type pacman &> /dev/null; then
+elif type pacman &> /dev/null; then
   yes | sudo pacman -S zsh
 fi
 
