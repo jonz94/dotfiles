5414868bb3e2cefcb8a47b01389b393957f6ce93
jonz94 <jody16888@gmail.com>
2021-05-18 01:28:15 +0800

    🔁 refactor(zsh): use better method to update zimfw module

--- zsh/plugins/update.zsh
+++ zsh/plugins/update.zsh
@@ -6,9 +6,9 @@ dug() {
   git -C ${DOTS} pull
 
   echo ${BLUE}"[zim]" updating...${NORMAL}
-  zimfw install
-  zimfw update
-  zimfw upgrade
+  zsh ~/.zim/zimfw.zsh install
+  zsh ~/.zim/zimfw.zsh update
+  zsh ~/.zim/zimfw.zsh upgrade
 
   echo ${BLUE}"[tpm]" updating...${NORMAL}
   ~/.tmux/plugins/tpm/bin/update_plugins all
