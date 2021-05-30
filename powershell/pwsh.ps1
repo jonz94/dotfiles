0a322aff9a1fffe4381a07e62a414018d7a0333f
jonz94 <jody16888@gmail.com>
2021-05-30 16:18:52 +0800

    ✅ fix(powershell): resolve `gcm` alias issue

--- powershell/pwsh.ps1
+++ powershell/pwsh.ps1
@@ -1,3 +1,6 @@
 ﻿function gpoat { git push origin --all && git push origin --tags }
 function sud { scoop update && scoop status }
 function ud { scoop update && scoop status }
+
+Remove-Item Alias:\gcm -Force *> $null
+function gcm { git checkout main $args || git checkout master $args }
