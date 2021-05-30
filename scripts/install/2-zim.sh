13b6e5caca85af2beec6561ff823211375820d52
jonz94 <jody16888@gmail.com>
2021-05-30 17:13:15 +0800

    ✅ fix(bootstrap): add missing `backup` function when installing zim

--- scripts/install/2-zim.sh
+++ scripts/install/2-zim.sh
@@ -5,6 +5,17 @@ if [ -d ~/.zim ]; then
   exit 0
 fi
 
+backup() {
+  if [ -f ${1} ]; then
+    mkdir -p `dirname ${1}`
+
+    TIMESTAMP=`date +"%Y-%m-%d,%H:%M:%S"`
+
+    echo "Backup old ${1} to ${1}.backup.${TIMESTAMP}"
+    mv ${1} ${1}.backup.${TIMESTAMP}
+  fi
+}
+
 backup $HOME/.zshenv
 backup $HOME/.zshrc
 backup $HOME/.zlogin
