cec4f2a183251f6cd985116801c95f74ee0296c4
jonz94 <jody16888@gmail.com>
2021-06-03 18:06:36 +0800

    ✅ fix(tmux): add true color for tmux

--- shell/aliases/tmux.sh
+++ shell/aliases/tmux.sh
@@ -1,10 +1,6 @@
 # tmux command aliases
 
 if type tmux &> /dev/null; then
-  # Make sure to execute the following command:
-  # $ tic /path/to/dotfiles/scripts/screen-256color.terminfo
-  alias tmux='env TERM=screen-256color tmux'
-
   alias t='tmux attach 2> /dev/null || tmux new -s home'
   alias ta='tmux attach'
   alias tkall='tmux kill-server'
