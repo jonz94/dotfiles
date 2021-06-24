46043c7c5e594612f6abf1b1dc26950a13900f1e
jonz94 <jody16888@gmail.com>
2021-06-24 15:15:44 +0800

    📝 docs(wsl): add instruction for using sudo without password

--- wsl/_set_eth0_mtu_size_to_1480.sh
+++ wsl/_set_eth0_mtu_size_to_1480.sh
@@ -3,6 +3,13 @@
 # so we should also set eth0's mtu size to 1480
 # this will avoid a lot of network issues
 
+# we can add following two lines to sudoers file of WSL
+# this will allow us to execute `sudo ip link set eth0 mtu 1480` without password
+# Note: strongly recommend to use command `sudo visudo` for editing sudoers file
+
+# #Allow members of group sudo to execute this command without password
+# %sudo ALL=(root) NOPASSWD: /usr/sbin/ip link set eth0 mtu 1480
+
 if [[ -z `ip addr | grep "eth0.*mtu 1480 "` ]]; then
-  sudo ip link set eth0 mtu 1480
+  sudo /usr/sbin/ip link set eth0 mtu 1480
 fi
