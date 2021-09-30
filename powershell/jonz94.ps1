2f11d06522258310a7d241f528c980798e14db90
jonz94 <jody16888@gmail.com>
2021-09-30 10:14:31 +0800

    ✨ feat(powershell): add `yci` alias as yarn's equivalent of `npm ci`

--- powershell/jonz94.ps1
+++ powershell/jonz94.ps1
@@ -308,6 +308,12 @@ function ylso { yarn outdated }
 function yr { yarn run }
 function yrm { yarn remove }
 
+# equivalent of `npm ci`
+function yci {
+  rimraf node_modules
+  yarn install --frozen-lockfile
+}
+
 # quickly install prettier related packages via yarn
 function yap { yarn add --dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports typescript }
 
