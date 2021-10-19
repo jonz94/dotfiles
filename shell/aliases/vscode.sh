c692b91ac1cc5ee151a99e87b162f0c3b702aa8e
jonz94 <jody16888@gmail.com>
2021-10-19 15:48:25 +0800

    🔄 refactor(shell): simplify vscode related aliases

--- shell/aliases/vscode.sh
+++ shell/aliases/vscode.sh
@@ -1,23 +1,13 @@
 # vscode & vscode-insiders command aliases
 
 if type code &> /dev/null; then
-  if [ `uname -r | grep -i microsoft` ]; then
-    alias c.='code . &'
-    alias c,='code . &'
-  else
-    alias c.='code .'
-    alias c,='code .'
-  fi
+  alias c.='code .'
+  alias c,='code .'
 fi
 
 if type code-insiders &> /dev/null; then
-  if [ `uname -r | grep -i microsoft` ]; then
-    alias ci.='code-insiders . &'
-    alias ci,='code-insiders . &'
-  else
-    alias ci.='code-insiders .'
-    alias ci,='code-insiders .'
-  fi
+  alias ci.='code-insiders .'
+  alias ci,='code-insiders .'
 
   alias ci='code-insiders'
 fi
