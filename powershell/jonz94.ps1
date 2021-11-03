2933c80c08d719be6e1ced1200ff0e9bacba75ff
jonz94 <jody16888@gmail.com>
2021-11-03 15:22:42 +0800

    😎 feat(powershell): add `invoke-vcvarsall` function
    Setup visual studio command prompt environment variables
    for current powershell session

--- powershell/jonz94.ps1
+++ powershell/jonz94.ps1
@@ -1,4 +1,4 @@
-# UTF-8 for printing
+﻿# UTF-8 for printing
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 # Opt-out powershell telemetry
@@ -413,6 +413,47 @@ Get-ChildItem –Path "~/dotfiles/powershell/completions" -Recurse -Filter *.ps1
 
 # whatis
 function whatis { ((Get-Command $args[0]).Definition).Trim() }
+
+# setup visual studio command prompt environment variables for current powershell session
+# via invoking `vcvarsall.bat` provided by visual studio c++ build tools
+#
+# example usage 1:
+#     invoke-vcvarsall -Architecture "x64" -ShowLogs
+#
+# example usage 2:
+#     invoke-vcvarsall -Architecture "x64_arm64" -VcvarsallDir "D:\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build" -ShowLogs
+#
+# credit: https://stackoverflow.com/a/21652729/9979122
+function invoke-vcvarsall {
+  param(
+    [parameter(Mandatory, HelpMessage="See https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line#vcvarsall-syntax for available architecture")]
+    [ValidateSet("x86", "x86_amd64", "x86_x64", "x86_arm", "amd64", "x64", "amd64_x86", "x64_x86", "amd64_arm", "x64_arm", "amd64_arm64", "x64_arm64")]
+    [string]$Architecture,
+    [string]$VcvarsallDir = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build",
+    [switch]$ShowLogs
+  )
+
+  $vcvarsallPath = (Join-Path $VcvarsallDir "vcvarsall.bat")
+  if (!(Test-Path $vcvarsallPath)) {
+    Write-Host "[Error]: Invoke-VcVarsAll failed." -ForegroundColor Red
+    Write-Host "${vcvarsallPath}: No such file." -ForegroundColor Red
+    return
+  }
+
+  Push-Location $VcvarsallDir
+
+  cmd.exe /c "vcvarsall.bat ${Architecture} & set" | foreach {
+    if ($_ -match "(.*?)=(.*)") {
+      Set-Item -Force -Path "ENV:\$($matches[1])" -Value "$($matches[2])"
+    }
+  }
+
+  Pop-Location
+
+  if ($ShowLogs) {
+    Write-Host "Visual Studio Command Prompt variables set. (Architecture: ${Architecture})" -ForegroundColor Green
+  }
+}
 
 # add backwards compatibility for older powershell
 if ( $HOST.Version.Major -ge 7 ) {
