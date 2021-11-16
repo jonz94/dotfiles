735180b5b68a29ee591696c33510e31a323465bc
jonz94 <jody16888@gmail.com>
2021-11-17 04:13:28 +0800

    🔁 refactor(bootstrap.ps1): tweaks `git` setup

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,7 +1,7 @@
 ﻿# setting up git
-git config --global user.name "jonz94"
-git config --global user.email "jody16888@gmail.com"
-git config --global pull.rebase false
+git config --global user.name 'jonz94'
+git config --global user.email 'jody16888@gmail.com'
+git config --global pull.rebase true
 git config --global init.defaultBranch main
 
 # Create local profile if it does not exist
