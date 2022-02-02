b9f69f757dcbf49b4ece2adc95550f22e66ea9b8
jonz94 <jody16888@gmail.com>
2022-02-03 06:02:46 +0800

    ✨ feat(neovim): enhance lsp key mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -8,6 +8,17 @@ end
 
 local lsp_installer = require('nvim-lsp-installer')
 
+-- stylua: ignore start
+local function on_attach()
+  vim.keymap.set('n', 'K',          function() vim.lsp.buf.hover()      end, { buffer = 0 })
+  vim.keymap.set('n', '<Leader>gd', function() vim.lsp.buf.definition() end, { buffer = 0 })
+  vim.keymap.set('n', '<Leader>gr', function() vim.lsp.buf.references() end, { buffer = 0 })
+  vim.keymap.set('n', '<Leader>dj', function() vim.diagnostic.next()    end, { buffer = 0 })
+  vim.keymap.set('n', '<Leader>dk', function() vim.diagnostic.prev()    end, { buffer = 0 })
+  vim.keymap.set('n', '<Leader>r',  function() vim.lsp.buf.rename()     end, { buffer = 0 })
+end
+-- stylua: ignore end
+
 lsp_installer.settings({
   ui = {
     icons = {
@@ -19,26 +30,19 @@ lsp_installer.settings({
 })
 
 lsp_installer.on_server_ready(function(server)
-  local options = {}
+  local options = {
+    on_attach = on_attach,
+  }
 
   if server.name == 'sumneko_lua' then
     if pcall(require, 'nvim-lsp-installer') then
-      options = require('lua-dev').setup()
+      options = require('lua-dev').setup({
+        lspconfig = {
+          on_attach = on_attach,
+        },
+      })
     end
   end
 
   server:setup(options)
-end)
-
--- go to definition
-vim.keymap.set('n', '<Leader>gd', function()
-  vim.lsp.buf.definition()
-end)
-
-vim.keymap.set('n', '<Leader>dj', function()
-  vim.diagnostic.next()
-end)
-
-vim.keymap.set('n', '<Leader>dk', function()
-  vim.diagnostic.prev()
 end)
