1b01f43660d4b6971599c7fa20f3db016f0cbcd8
jonz94 <jody16888@gmail.com>
2022-02-03 06:11:31 +0800

    ✨ feat(neovim): enhance indent blankline settings

--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -10,4 +10,7 @@ require('indent_blankline').setup({
   char = '│',
   buftype_exclude = { 'terminal', 'nofile', 'help' },
   space_char_blankline = ' ',
+  show_current_context = true,
+  -- show_current_context_start = true,
+  context_highlight_list = { 'Normal' },
 })
