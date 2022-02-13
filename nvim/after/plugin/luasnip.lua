bf3aaa787a40eb14c58f23228620c84a7164d09c
jonz94 <jody16888@gmail.com>
2022-02-13 08:40:38 +0800

    😎 feat(neovim): setup luasnip's load snippets from vscode feature

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -8,19 +8,31 @@ end
 
 local luasnip = require('luasnip')
 
+-- from https://github.com/L3MON4D3/LuaSnip/blob/eb84bb89933141fa0cd0683cb960fef975106dfd/lua/luasnip/loaders/from_vscode.lua#L118-L122
+local function get_snippet_runtimepath()
+  return vim.tbl_map(function(item)
+    return vim.fn.fnamemodify(item, ':h')
+  end, vim.api.nvim_get_runtime_file('package.json', true))
+end
+
 luasnip.config.set_config({
   history = true,
   -- update more often, :h events for more info.
   updateevents = 'TextChanged,TextChangedI',
 })
 
-require('luasnip.loaders.from_vscode').load()
+-- load snippets from other plugins (e.g. rafamadriz/friendly-snippets) and vscode
+local snippets_provided_by_other_vim_plugins = get_snippet_runtimepath()
+local snippets_from_my_vscode = { '~/dotfiles/vscode' }
+local paths = vim.list_extend(snippets_provided_by_other_vim_plugins, snippets_from_my_vscode)
+require('luasnip.loaders.from_vscode').load({ paths = paths })
 
 vim.cmd([[
   imap <silent><expr> <c-j> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<c-j>'
   inoremap <silent> <c-k> <cmd>lua require('luasnip').jump(-1)<CR>
 
-  imap <silent><expr> <C-l> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-l>'
+  imap <silent><expr> <c-l> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<c-l>'
+  imap <silent><expr> <c-h> luasnip#choice_active() ? '<Plug>luasnip-prev-choice' : '<c-h>'
 
   snoremap <silent> <c-j> <cmd>lua require('luasnip').jump(1)<CR>
   snoremap <silent> <c-k> <cmd>lua require('luasnip').jump(-1)<CR>
