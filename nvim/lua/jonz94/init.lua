77c36eabdddcf6df0bce7978e5e06cc9a1fe6f3c
jonz94 <jody16888@gmail.com>
2022-04-18 00:24:58 +0800

    🔥 chore(neovim): remove `astronauta.nvim` related settings

--- nvim/lua/jonz94/init.lua
+++ nvim/lua/jonz94/init.lua
@@ -15,8 +15,5 @@ require('jonz94.disable-builtin')
 -- use `space` as leader key
 vim.g.mapleader = ' '
 
--- force loading of astronauta first
-vim.cmd([[runtime plugin/astronauta.vim]])
-
 -- load plugins
 require('jonz94.load-plugins')
