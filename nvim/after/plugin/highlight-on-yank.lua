56b8259c2ee163b0ef1f60ef82c3e40fa6a01a16
jonz94 <jody16888@gmail.com>
2022-05-10 00:53:09 +0800

    🔁 refactor(neovim): simplify highlight on yank implementation

--- nvim/after/plugin/highlight-on-yank.lua
+++ nvim/after/plugin/highlight-on-yank.lua
@@ -1,6 +1,10 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    require('jonz94.on-yank').highlight()
+    vim.highlight.on_yank({
+      higroup = 'Substitute',
+      timeout = 150,
+      on_macro = true,
+    })
   end,
   group = vim.api.nvim_create_augroup('HighlightOnYank', { clear = true }),
 })
