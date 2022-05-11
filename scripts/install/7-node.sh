4b444876f39740d04a1e457950e00df7e3c08d4d
jonz94 <jody16888@gmail.com>
2022-05-12 05:47:33 +0800

    🔁 refactor(bootstrap.sh): minor refactor

--- scripts/install/7-node.sh
+++ scripts/install/7-node.sh
@@ -1,23 +1,25 @@
+echo 'installing node...'
+
 if [ $(uname -r | grep -i ish) ]; then
   echo 'installing node via apk'
   apk add nodejs-current nodejs-current-doc npm
   exit 0
-else
-  echo 'installing node via fnm'
-
-  export PATH="$HOME/.local/share/fnm:$PATH"
-  eval "$(fnm env)"
-
-  fnm install --lts
-
-  fnm alias lts-latest default
-
-  echo 'updating npm to latest version'
-  npm i -g npm@latest
-
-  echo 'installing yarn via npm'
-  npm i -g yarn@latest
-
-  echo 'removing global corepack package'
-  npm rm -g corepack
 fi
+
+echo 'installing node via fnm'
+
+export PATH="$HOME/.local/share/fnm:$PATH"
+eval "$(fnm env)"
+
+fnm install --lts
+
+fnm alias lts-latest default
+
+echo 'updating npm to latest version'
+npm i -g npm@latest
+
+echo 'installing yarn via npm'
+npm i -g yarn@latest
+
+echo 'removing global corepack package'
+npm rm -g corepack
