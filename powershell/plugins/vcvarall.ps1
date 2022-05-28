9198a64fc8ddfd798dbd7248aa6b4458bd8a2a23
jonz94 <jody16888@gmail.com>
2022-05-28 15:10:17 +0800

    ✨ feat(powershell): update `vcvarall.ps1` to use VS2022 as default

--- powershell/plugins/vcvarall.ps1
+++ powershell/plugins/vcvarall.ps1
@@ -5,15 +5,18 @@
 #     invoke-vcvarsall -Architecture "x64" -ShowLogs
 #
 # example usage 2:
+#     invoke-vcvarsall -VcvarsallDir "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build"
+#
+# example usage 3:
 #     invoke-vcvarsall -Architecture "x64_arm64" -VcvarsallDir "D:\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build" -ShowLogs
 #
 # credit: https://stackoverflow.com/a/21652729/9979122
 function invoke-vcvarsall {
   param(
     [parameter(Mandatory, HelpMessage = 'See https://docs.microsoft.com/en-us/cpp/build/building-on-the-command-line#vcvarsall-syntax for available architecture')]
-    [ValidateSet('x86', 'x86_amd64', 'x86_x64', 'x86_arm', 'amd64', 'x64', 'amd64_x86', 'x64_x86', 'amd64_arm', 'x64_arm', 'amd64_arm64', 'x64_arm64')]
+    [ValidateSet('x86', 'x86_amd64', 'x86_x64', 'x86_arm', 'x86_arm64', 'amd64', 'x64', 'amd64_x86', 'x64_x86', 'amd64_arm', 'x64_arm', 'amd64_arm64', 'x64_arm64')]
     [string] $Architecture,
-    [string] $VcvarsallDir = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build",
+    [string] $VcvarsallDir = "${Env:ProgramFiles}\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build",
     [switch] $ShowLogs
   )
 
