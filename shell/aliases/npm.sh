6f5862ed2186a76541fbaabf79c0f309e2be2fb5
jonz94 <jody16888@gmail.com>
2022-06-02 22:21:02 +0800

    😎 feat(shell): replace `-g` with `--location=global` for npm >= 8

--- shell/aliases/npm.sh
+++ shell/aliases/npm.sh
@@ -3,15 +3,44 @@
 if type npm &> /dev/null; then
   alias nci='npm ci'
   alias nid='npm i --save-dev'
-  alias nig='npm i -g'
   alias nis='npm i --save'
   alias nls='npm ls --depth 0'
-  alias nlsg='npm ls --depth 0 -g'
   alias nlso='npm outdated'
   alias nr='npm run'
   alias nrm='npm uninstall'
-  alias nug='npm update -g'
 
   # Always typo nr into br...
   alias br='npm run'
+
+  __npm_major_version () {
+    npm -v | cut -d. -f1
+  }
+
+  nig () {
+    if [ "$#" -eq 0 ]; then
+      return 1
+    fi
+
+    if [ "$(__npm_major_version)" -ge 8 ]; then
+      npm i --location=global "$@"
+    else
+      npm i -g "$@"
+    fi
+  }
+
+  nlsg () {
+    if [ "$(__npm_major_version)" -ge 8 ]; then
+      npm ls --depth 0 --location=global
+    else
+      npm ls --depth 0 -g
+    fi
+  }
+
+  nug () {
+    if [ "$(__npm_major_version)" -ge 8 ]; then
+      npm update --location=global "$@"
+    else
+      npm update -g "$@"
+    fi
+  }
 fi
