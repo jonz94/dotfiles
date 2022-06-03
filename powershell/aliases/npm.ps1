28a1960d3ab15ac987ad7594f2d29ec5cc5f6a28
jonz94 <jody16888@gmail.com>
2022-06-03 15:31:32 +0800

    🤣 fix(powershell): finally, the cause of aliases issue is found, and fixed!

--- powershell/aliases/npm.ps1
+++ powershell/aliases/npm.ps1
@@ -1,16 +1,16 @@
 # npm aliases
 function nci { npm ci }
-function nid { npm install --save-dev $args }
-function nig { npm install -g $args }
-function nis { npm install --save $args }
+function nid { npm install --save-dev @args }
+function nig { npm install -g @args }
+function nis { npm install --save @args }
 function nls { npm ls --depth 0 }
 function nlsg { npm ls --depth 0 -g }
 function nlso { npm outdated }
-function nr { npm run $args }
-function nrm { npm uninstall $args }
+function nr { npm run @args }
+function nrm { npm uninstall @args }
 
 # always typo br
-function br { npm run $args }
+function br { npm run @args }
 
 # quickly install prettier related packages via npm
 function nip { npm install --save-dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports typescript }
