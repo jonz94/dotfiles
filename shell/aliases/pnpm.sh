e6c690e0737c3664a6a95ccae9fc610a782eda0f
jonz94 <jody16888@gmail.com>
2022-06-24 00:13:24 +0800

    ✨ feat(shell): tweak aliases for `pnpm`

--- shell/aliases/pnpm.sh
+++ shell/aliases/pnpm.sh
@@ -1,9 +1,9 @@
 # pnpm command aliases
 
 if type pnpm &> /dev/null; then
-  alias pid='pnpm i --save-dev'
-  alias pis='pnpm i --save'
-  alias plsall='pnpm ls --depth Infinity'
-  alias plso='pnpm outdated'
-  alias prm='pnpm remove'
+  alias pnid='pnpm i --save-dev'
+  alias pnis='pnpm i --save'
+  alias pnlsall='pnpm ls --depth Infinity'
+  alias pnlso='pnpm outdated'
+  alias pnrm='pnpm remove'
 fi
--- zsh/plugins/setup-prettier.zsh
+++ zsh/plugins/setup-prettier.zsh
@@ -5,6 +5,11 @@ nip() {
   npm i --save-dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports @types/prettier
 }
 
+# pnpm
+pnip() {
+  pnpm i --save-dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports @types/prettier
+}
+
 # yarn
 yap() {
   yarn add --dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports @types/prettier
