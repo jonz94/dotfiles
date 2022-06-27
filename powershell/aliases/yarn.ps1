7b9844ebee05504dc31a48f9b1fd43a2238f0029
jonz94 <jody16888@gmail.com>
2022-06-28 06:01:41 +0800

    ✅ fix(powershell): fix `yarn` related aliases

--- powershell/aliases/yarn.ps1
+++ powershell/aliases/yarn.ps1
@@ -1,9 +1,9 @@
 # yarn aliases
-function ya { yarn add }
-function yad { yarn add --dev }
+function ya { yarn add @args }
+function yad { yarn add --dev @args }
 function ylso { yarn outdated }
-function yr { yarn run }
-function yrm { yarn remove }
+function yr { yarn run @args }
+function yrm { yarn remove @args }
 
 # equivalent of `npm ci`
 function yci {
