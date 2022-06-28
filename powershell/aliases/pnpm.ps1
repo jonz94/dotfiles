33e9ae7125fb48ccff09437392ce6926e8cd7218
jonz94 <jody16888@gmail.com>
2022-06-28 10:51:02 +0800

    ✅ fix(powershell): fix `pnci`  alias

--- powershell/aliases/pnpm.ps1
+++ powershell/aliases/pnpm.ps1
@@ -1,7 +1,12 @@
 # pnpm aliases
-function pnci { pnpm ci }
 function pnid { pnpm i --save-dev @args }
 function pnis { pnpm i --save @args }
 function pnlsall { pnpm ls --depth Infinity }
 function pnlso { pnpm outdated }
 function pnrm { pnpm remove @args }
+
+# equivalent of `npm ci`
+function pnci {
+  rimraf node_modules
+  pnpm install --frozen-lockfile
+}
