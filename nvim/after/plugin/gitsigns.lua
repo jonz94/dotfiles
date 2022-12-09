4875f1c48566babd5f0ccb249436645dff21c744
jonz94 <jody16888@gmail.com>
2022-12-10 04:26:02 +0800

    ✨ feat(neovim): add `gitsigns` key mappings

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -22,4 +22,54 @@ gitsigns.setup({
       linehl = 'GitSignsChangeLn',
     },
   },
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        return ']c'
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        return '[c'
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return '<Ignore>'
+    end, { expr = true })
+
+    -- Actions
+    map({ 'n', 'v' }, '<Leader>hs', '<Cmd>Gitsigns stage_hunk<CR>')
+    map({ 'n', 'v' }, '<Leader>hr', '<Cmd>Gitsigns reset_hunk<CR>')
+    map('n', '<Leader>hS', gs.stage_buffer)
+    map('n', '<Leader>hu', gs.undo_stage_hunk)
+    map('n', '<Leader>hR', gs.reset_buffer)
+    map('n', '<Leader>hp', gs.preview_hunk)
+    map('n', '<Leader>hb', function()
+      gs.blame_line({ full = true })
+    end)
+    map('n', '<Leader>tb', gs.toggle_current_line_blame)
+    map('n', '<Leader>hd', gs.diffthis)
+    map('n', '<Leader>hD', function()
+      gs.diffthis('~')
+    end)
+    map('n', '<Leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({ 'o', 'x' }, 'ih', '<Cmd><C-U>Gitsigns select_hunk<CR>')
+  end,
 })
