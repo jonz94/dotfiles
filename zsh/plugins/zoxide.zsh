2850a4c9c6195761ae545894c18c60108da32972
jonz94 <jody16888@gmail.com>
2023-03-14 00:23:41 +0800

    🔁 refactor(zsh): remove workaround for `zoxide`

--- zsh/plugins/zoxide.zsh
+++ zsh/plugins/zoxide.zsh
@@ -2,17 +2,4 @@
 
 if type zoxide &> /dev/null; then
   eval "$(zoxide init zsh)"
-
-  # TODO: this part can be removed after zoxide release a new version
-  # credit: https://github.com/ajeetdsouza/zoxide/pull/474
-  # overwrite `__zoxide_cd` function to trigger zsh's `chpwd` hook
-  if [ "${_ZO_ECHO}" = "1" ]; then
-    function __zoxide_cd() {
-      \builtin cd -- "$@" && __zoxide_pwd
-    }
-  else
-    function __zoxide_cd() {
-      \builtin cd -- "$@"
-    }
-  fi
 fi
