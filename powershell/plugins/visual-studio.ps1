a993b0ae39710f0be5b46ed3a2b3dba2ac09927c
jonz94 <jody16888@gmail.com>
2023-03-22 19:30:28 +0800

    🚀 perf(powershell): use `fd` to improve `vs.` alias speed

--- powershell/plugins/visual-studio.ps1
+++ powershell/plugins/visual-studio.ps1
@@ -1,5 +1,9 @@
 function vs. {
-  $solutions = $(Get-ChildItem -Filter *.sln -Recurse | ForEach-Object { $_.FullName })
+  if ((Get-Command fd -ErrorAction SilentlyContinue).Count -gt 0) {
+    $solutions = $(fd --absolute-path ".sln")
+  } else {
+    $solutions = $(Get-ChildItem -Filter *.sln -Recurse | ForEach-Object { $_.FullName })
+  }
 
   if ($solutions.Count -lt 1) {
     Write-Host 'No .sln file was found' -ForegroundColor Red
