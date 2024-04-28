e67d6815b3a242b93f9b9f6d015a14579969d4d8
jonz94 <jody16888@gmail.com>
2024-04-28 08:56:19 +0800

    ✅ fix(powershell): fix script to clean up winget cache

--- powershell/plugins/winget.ps1
+++ powershell/plugins/winget.ps1
@@ -1,4 +1,4 @@
 function clean-up-winget-cache {
-  Remove-Item -Force -Recurse "${$env:LOCALAPPDATA}\Temp\WinGet"
+  Remove-Item -Force -Recurse "$env:LOCALAPPDATA\Temp\WinGet" -ErrorAction SilentlyContinue
   Write-Host 'Done! 🎉'
 }
