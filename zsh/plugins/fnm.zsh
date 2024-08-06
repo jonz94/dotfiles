128c0c07aececb9af8333d50fddd2856f224fc00
jonz94 <jody16888@gmail.com>
2024-08-06 16:42:42 +0800

    ✨ feat(zsh): add `--version-file-strategy=recursive` flag when initializing fnm

--- zsh/plugins/fnm.zsh
+++ zsh/plugins/fnm.zsh
@@ -1,10 +1,11 @@
 # fast node version manager
+# https://github.com/Schniz/fnm
 
 export FNM_DIR="$HOME/.local/share/fnm"
 
 if (( $+commands[fnm] )); then
-  eval "$(fnm env --use-on-cd)"
+  eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
 elif [ -s "$FNM_DIR/fnm" ]; then
   export PATH="$FNM_DIR:$PATH"
-  eval "$(fnm env --use-on-cd)"
+  eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
 fi
