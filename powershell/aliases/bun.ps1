4c9c4c71b8ac9d40ed44706640d07bdfe076d8f2
jonz94 <jody16888@gmail.com>
2024-09-30 07:31:25 +0800

    ✨ feat(powershell): add aliases for `bun`

---
+++ powershell/aliases/bun.ps1
@@ -0,0 +1,16 @@
+# bun aliases
+function bid { bun add --dev @args }
+function bis { bun add @args }
+function bls { bun pm ls }
+function blsall { bun pm ls --all }
+function blso { bun outdated }
+function brm { bun remove @args }
+
+# equivalent of `npm ci`
+function bci {
+  if (Test-Path node_modules) {
+    Remove-Item -Recurse node_modules
+  }
+
+  bun install --frozen-lockfile
+}
