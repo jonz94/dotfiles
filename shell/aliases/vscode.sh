6574cbf5e05d9befb6158141b38688808485a5ab
jonz94 <jody16888@gmail.com>
2024-10-21 11:56:11 +0800

    ✨ feat(alias): add `c..` alias

--- shell/aliases/vscode.sh
+++ shell/aliases/vscode.sh
@@ -3,6 +3,11 @@
 if type code &> /dev/null; then
   alias c.='code .'
   alias c,='code .'
+
+  alias c..='code ..'
+  alias c,,='code ..'
+  alias c.,='code ..'
+  alias c,.='code ..'
 fi
 
 if type code-insiders &> /dev/null; then
