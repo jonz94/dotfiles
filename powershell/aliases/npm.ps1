eb5045c9a9facaeba3d600e6a42825211f5c66ff
jonz94 <jody16888@gmail.com>
2025-02-08 01:10:18 +0800

    ✨ feat(alias): update `nlsg` alias for powershell

--- powershell/aliases/npm.ps1
+++ powershell/aliases/npm.ps1
@@ -4,7 +4,7 @@ function nid { npm install --save-dev @args }
 function nig { npm install -g @args }
 function nis { npm install --save @args }
 function nls { npm ls --depth 0 }
-function nlsg { npm ls --depth 0 -g }
+function nlsg { node -v; npm ls --depth 0 -g }
 function nlso { npm outdated }
 function nr { npm run @args }
 function nrm { npm uninstall @args }
