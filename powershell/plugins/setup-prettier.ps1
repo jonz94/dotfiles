0e46c5ffc19e269985ac2c5eefe17e40c41a0888
jonz94 <jody16888@gmail.com>
2025-04-11 04:36:14 +0800

    😎 feat(powershell): update setup prettier script using `@antfu/ni`

--- powershell/plugins/setup-prettier.ps1
+++ powershell/plugins/setup-prettier.ps1
@@ -1,10 +1,4 @@
 # setup prettier
 
-# npm
-function nip { npm install --save-dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports @types/prettier }
-
-# pnpm
-function pnip { pnpm install --save-dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports @types/prettier }
-
-# yarn
-function yap { yarn add --dev prettier prettier-plugin-packagejson prettier-plugin-organize-imports @types/prettier }
+# @antfu/ni
+function nip { ni -D prettier prettier-plugin-packagejson prettier-plugin-organize-imports prettier-plugin-tailwindcss }
