f42ef1254714e1e514e98f91e5716dcc28a671ab
jonz94 <jody16888@gmail.com>
2025-04-15 14:14:47 +0800

    ✨ feat(powershell): improve `pnpm` related aliases

--- powershell/aliases/pnpm.ps1
+++ powershell/aliases/pnpm.ps1
@@ -1,8 +1,8 @@
 # pnpm aliases
 function pnid { pnpm i --save-dev @args }
 function pnis { pnpm i --save @args }
-function pnlsall { pnpm ls --depth Infinity }
-function pnlso { pnpm outdated }
+function pnlsall { pnpm ls --depth Infinity @args }
+function pnlso { pnpm outdated @args }
 
 # equivalent of `npm ci`
 function pnci {
